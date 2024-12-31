38a54d61c46629ff82dc362883b0a772fc5b4572
Rahul <rahulbodh819@gmail.com>
2024-12-31 17:09:45 +0530

    add dependency for loader

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        dataBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +49,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("com.github.EudyContreras:Skeleton-Bones:1.4.5")
 }
